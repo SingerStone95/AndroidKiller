50d8eb3103b76ff161aaba3513dc1b7870991462
yogachen <yogachen@tencent.com>
2023-02-02 14:58:16 +0800

    适配AndroidKiller

--- bin/apktool/apktool.bat
+++ bin/apktool/apktool.bat
@@ -1,4 +1,16 @@
 @echo off
 if "%PATH_BASE%" == "" set PATH_BASE=%PATH%
 set PATH=%CD%;%PATH_BASE%;
+echo %PATH%
+echo %PATH_BASE%
+echo %1
+echo %2
+echo %3
+echo %4
+echo %5
+echo %6
+echo %7
+echo %8
+echo %9
+
 java -jar "%~dp0\apktool\apktool_2.7.0.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
--- bin/apktool/signapk.bat
+++ bin/apktool/signapk.bat
@@ -1,2 +1,3 @@
 @echo off
+REM java -jar "%~dp0\apksigner.jar" "%~dp0\testkey.x509.pem" "%~dp0\testkey.pk8" %1 %2
 java -jar "%~dp0\apksigner.jar" sign --v3-signing-enabled false --ks "%~dp0\singerstone.jks"  --ks-key-alias singerstone --ks-pass pass:cbh123456 --key-pass pass:cbh123456  --out %2  %1
